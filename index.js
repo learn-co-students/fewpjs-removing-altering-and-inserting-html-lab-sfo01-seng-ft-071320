cc004b652563112f83d7412cbdc4d744f340ee56
Taci Shlosberg <tshlosberg@gmail.com>
2020-08-24 13:31:59 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code here!
+
+//no longer has DOM node 'main#main'
+document.getElementById("main").remove();
+
+//has a 'newHeader' variable that points to node 'h1#victory'
+let newHeader = document.createElement("h1");
+
+//has a 'newHeader' variable that points to node 'h1#victory'
+let att = document.createAttribute("id");
+att.value = "victory"
+newHeader.setAttributeNode(att);
+
+//has a 'newHeader' variable that points to node 'h1#victory' with "YOUR-NAME is the champion" inside
+newHeader.textContent = "Taci is the champion";
+
+console.log(newHeader);
